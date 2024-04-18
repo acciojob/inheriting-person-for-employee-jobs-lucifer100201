3c0164b0ba19a8c8d75edd0b5decccc76dafaa4c
lucifer100201 <99162740+lucifer100201@users.noreply.github.com>
2024-04-18 03:40:22 -0600

    created a submission

--- script.js
+++ script.js
@@ -18,9 +18,7 @@ class Employee extends Person{
 		console.log(`Hello, my name is ${this._name}, I am ${this._age} years old, and my job title is ${this._jobTitle}.`)
 	}
 }
-function Person(name, age) {}
 
-function Employee(name, age, jobTitle) {}
 
 // Do not change code below this line
 window.Person = Person;
