2595fb45760af31f1b00e3103b77d52f52b4073c
lucifer100201 <99162740+lucifer100201@users.noreply.github.com>
2024-04-18 03:38:10 -0600

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
 // complete this js code
+class Person {
+	constructor(name,age){
+		this._name=name;
+		this._age=age;
+	}
+	greet(){
+		console.log(`Hello, my name is ${this._name}, I am ${this._age} years old.`)
+	}
+}
+
+class Employee extends Person{
+	constructor(name,age,jobTitle){
+		super(name,age);
+		this._jobTitle=jobTitle; 
+	}
+	jobGreet(){
+		console.log(`Hello, my name is ${this._name}, I am ${this._age} years old, and my job title is ${this._jobTitle}.`)
+	}
+}
 function Person(name, age) {}
 
 function Employee(name, age, jobTitle) {}
